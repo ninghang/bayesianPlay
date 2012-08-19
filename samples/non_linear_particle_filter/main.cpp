155e2e3c198aad90d5ad7414262fccbe1fec6dc5
Ninghang Hu <huninghang@gmail.com>
2012-08-19 17:14:16 +0200

    created eclipse project

--- samples/non_linear_particle_filter/main.cpp
+++ samples/non_linear_particle_filter/main.cpp
@@ -9,35 +9,35 @@ int main(int argc, char **argv)
   int i;
   int j;
   vector<Weighted_Sample> cloud;
-  
-  ofstream file_c("../data/cloud.dat"); // To save the cloud
-  ofstream file_s("../data/state.dat"); // To save the state
-  
-  Plane plane("../data/map_2.dat"); // The plane model
+
+  ofstream file_c("data/cloud.dat"); // To save the cloud
+  ofstream file_s("data/state.dat"); // To save the state
+
+  Plane plane("data/map_2.dat"); // The plane model
   G_Simulator sim(&plane); // To simulate a this model
-  
-  Bootstrap_Filter filter(100000,&plane); // A bootstrap filter to estimate the position
-  
+
+  Bootstrap_Filter filter(100000, &plane); // A bootstrap filter to estimate the position
+
   sim.Simulate(150); // simulation of 250 samples
-  sim.Save_Y("../data/output.dat"); // save the output
+  sim.Save_Y("data/output.dat"); // save the output
   // Here Init() and Update methods are used
   // for filtering because we want to get the cloud
   // at each step and save it in cloud.dat
   filter.Init(); // Initialization of the boostrap filter
-  for (k=0; k<sim.Y.size(); k++)
+  for (k = 0; k < sim.Y.size(); k++)
   {
-    cloud=filter.CloudGet(); // The current cloud is return
-    for(i=0; i<cloud.size(); i++) // and here it is saved
+    cloud = filter.CloudGet(); // The current cloud is return
+    for (i = 0; i < cloud.size(); i++) // and here it is saved
     {
-      for(j=0; j<cloud[i].Value.l; j++)
-        file_c<<cloud[i].Value(j)<<" ";
-      file_c<<endl;
+      for (j = 0; j < cloud[i].Value.l; j++)
+        file_c << cloud[i].Value(j) << " ";
+      file_c << endl;
     }
-    for (j=0; j<sim.X[k].l; j++) // The state is also saved
-      file_s<<sim.X[k](j)<<" ";
-    file_s<<endl<<endl<<endl;
-    file_c<<endl<<endl;
-    if( filter.Update(sim.Y[k])) // The filter is then update with a new observation
+    for (j = 0; j < sim.X[k].l; j++) // The state is also saved
+      file_s << sim.X[k](j) << " ";
+    file_s << endl << endl << endl;
+    file_c << endl << endl;
+    if (filter.Update(sim.Y[k])) // The filter is then update with a new observation
       filter.Init();
   }
   file_c.close();
--- samples/non_linear_particle_filter/plane.h
+++ samples/non_linear_particle_filter/plane.h
@@ -3,22 +3,22 @@
 
 #include <bfilt/gaussian_model.h>
 
-class Plane : public Gaussian_Nonlinear_Model
+class Plane: public Gaussian_Nonlinear_Model
 {
-  vector<double> Map;
-  double xmin;
-  double xmax;
-  double ymin;
-  double ymax;
+    vector<double> Map;
+    double xmin;
+    double xmax;
+    double ymin;
+    double ymax;
 
-  double sigv;
-  double sigc;
+    double sigv;
+    double sigc;
 
-public :
-  Plane(const char *filename);
-  
-  dcovector State_Function(const dcovector &X, const dcovector &W);
-  dcovector Observation_Function(const dcovector & X);
+  public:
+    Plane(const char *filename);
+
+    dcovector State_Function(const dcovector &X, const dcovector &W);
+    dcovector Observation_Function(const dcovector & X);
 };
 
 #endif
